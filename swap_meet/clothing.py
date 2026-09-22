cf2305decd2b14f379e949d7598975140ead9e11
porselvi <porselvi8989@gmail.com>
2026-09-22 10:34:21 -0700

    added wave4

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,3 +1,11 @@
+from swap_meet.item import Item
+import uuid
 class Clothing:
-    def __init__(self,id,fabric):
-        
+    def __init__(self,id,fabric = "unknown"):
+        self.fabric = fabric
+        self.id = uuid.uuid4().int
+    def get_category(self):
+        return "Clothing"
+    def __str__(self):
+        return f"An object of type Clothing with id {self.id}. It is made from {self.fabric} fabric."
+
--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,2 +1,8 @@
+import uuid
 class Decor:
-    pass
+    def __init__(self,width = 0,length = 0):
+        self.width = width
+        self.length = length 
+        self.id = uuid.uuid4().int
+    def get_category (self):
+        return "Decor"
