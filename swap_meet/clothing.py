c7cb23d677b823de887d3a84d0598e26f85e199c
porselvi <porselvi8989@gmail.com>
2026-09-22 08:39:55 -0700

    added till wave 4

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,3 @@
 class Clothing:
-    pass
+    def __init__(self,id,fabric):
+        
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,41 @@
 class Vendor:
-    pass
+    def __init__(self,inventory = None):
+        self.inventory = [] if inventory is None else inventory
+
+    def add(self,item):
+        self.inventory.append(item)
+        return item
+
+    def remove(self,item):
+        if item in self.inventory:
+            self.inventory.remove(item)
+            return item
+        return None
+    
+    def get_by_id(self,item_id):
+        for item in self.inventory:
+            if item.id == item_id:
+                return item
+        return None
+
+    def swap_items(self,other_vendor,my_item,their_item):
+        if my_item not in self.inventory or their_item not in other_vendor.inventory:
+            return False
+        self.remove(my_item)
+        other_vendor.add(my_item)
+
+        other_vendor.remove(their_item)
+        self.add(their_item)
+        return True
+
+    def swap_first_item(self,other_vendor):
+        if not self.inventory or not other_vendor.inventory:
+            return False
+        my_item = self.inventory[0]
+        friend_item = other_vendor.inventory[0]
+        self.remove(my_item)
+        other_vendor.add(my_item)
+
+        other_vendor.remove(friend_item)
+        self.add(friend_item)
+        return True
